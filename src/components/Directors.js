bda4d0141224f78f30b95cd4cf7d92013c36d5d3
Elena-Weber <crochetmiracles@gmail.com>
2021-06-30 11:47:59 -0400

    commit tree 439f91cbabcd3cbe19d332aa3aefc8e70878bbba

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,6 +4,18 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
+    <h1>Directors Page</h1>
+    {directors.map((dir, index) => (
+          <div key={index}>
+              <h3>Name: {dir.name}</h3>
+              <p>Movies:</p>
+              <ul>
+                  {dir.movies.map((movie, index) => (
+                      <li key={index}>{movie}</li>
+                  ))}
+              </ul>
+          </div>
+      ))}
       {/*{code here}*/}
     </div>
   );
